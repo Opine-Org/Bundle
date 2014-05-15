e86cab82b80fd70d8dbeacb42c8f1a478fa4a1bb
rmahoney-bsd <rmahoney@bluestatedigital.com>
2014-05-15 15:30:38 -0400

    not ready, sorry

--- src/BundleRoute.php
+++ src/BundleRoute.php
@@ -69,7 +69,7 @@ class BundleRoute {
             }
             $this->formRoute->json($bundleName);          
             $this->formRoute->app($bundleRoot, $bundleName);
-            $this->collectionRoute->json($bundleRoot);
+            //$this->collectionRoute->json($bundleRoot);
             $className = $root . '/../bundles/' . $bundleName . '/Route.php';
             if (!file_exists($className)) {
                 continue;
